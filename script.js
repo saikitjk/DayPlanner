af86d70992f4d2342315c124f5499e440fb8b058
JK <saikitjk@gmail.com>
2020-07-12 19:40:09 -0700

    rebuild UI and CSS

--- script.js
+++ script.js
@@ -13,10 +13,19 @@ $(document).ready(function(){
     setInterval(headerClock,1000);
     //Time date on the header
 
+
+
+
+});
+
+/*
+    var eventArray= [];
+
     //generate textbox
     function genTextbox(){
         var ampm = "";
         for (var hour = 9; hour <= 17; hour++){
+            var i = hour - 9;
             //console.log(hour);
             if (hour > 12) { 
                 ampm = "pm";
@@ -29,36 +38,98 @@ $(document).ready(function(){
             var timeStamp = $("<div>");
             timeStamp.attr("type", "text");
             timeStamp.text(hour + " " + ampm);
-            
             timeStamp.attr("class", "timeStamp");
-            //timeStamp.css("width","100%");
-            
             $("#timeStamp").append(timeStamp); 
           
             //input area
             var inputBox = $("<input>");
-            inputBox.attr("type", "textarea");
-            inputBox.attr("value", "");
+            inputBox.attr("type", "text");
             inputBox.attr("class", "eventBox");
-            //inputBox.css("width","100%");
             $("#eventBox").append(inputBox);
 
             //save button
             var saveButton = $("<button>");
             saveButton.attr("type", "button");
             saveButton.attr("value", "Save");
+            saveButton.attr("save-id", i);
             saveButton.attr("class", "saveButton");
-            //saveButton.css("width","100%");
             $("#saveButton").append(saveButton);
+
+        }
+
+
+            $("#saveButton").on("click", function(event){
+                event.preventDefault();  
+                var event2Save = document.getElementById("eventBox").value;
+                console.log(event2Save);
+                if(event2Save === ""){
+                    return; //check if tany data
+                }
+
+                var index = $(this).attr("save-id");
+                
+    
+                eventArray.push(event2Save);
+                console.log(eventArray);
+    
+                //store to local
+                localStorage.setItem("eventArray", JSON.stringify(eventArray));
+    
+            });
        
          
+        
+    }
+    genTextbox();
+
+*/
+
+/*
+    var ampm = "";
+    for (var hour = 9; hour <= 17; hour++){
+        var i = hour - 9;
+        //console.log(hour);
+        if (hour > 12) { 
+            ampm = "pm";
+        } 
+        else {
+            ampm = "am";
         }
+        /////////
+        var eventRow = $("<div>");
+        
+
+
+        //timestamp
+        console.log(ampm);
+        var timeStamp = $("<div>");
+        timeStamp.attr("type", "text");
+        timeStamp.text(hour + " " + ampm);
+        timeStamp.attr("class", "timeStamp");
+        $("#timeStamp").append(timeStamp); 
+    
+        //input area
+        var inputBox = $("<input>");
+        inputBox.attr("type", "text");
+        inputBox.attr("class", "eventBox");
+        $("#eventBox").append(inputBox);
+
+        //save button
+        var saveButton = $("<button>");
+        saveButton.attr("type", "button");
+        saveButton.attr("value", "Save");
+        saveButton.attr("save-id", i);
+        saveButton.attr("class", "saveButton");
+        $("#saveButton").append(saveButton);
+
     }
 
-    genTextbox();
+   
+
     
     
-    //Store to local
+    
+   
 
     //retrive from local
 
@@ -67,8 +138,4 @@ $(document).ready(function(){
     //
 
 
-
-});
-
-//<textarea class="eventBox col-md-9 description" id='eventBox'></textarea> <!--data-hour='9'-->
-//<button class="col-md-1 fas fa-save btn-block saveBtn" id="btn"></button>
+ */
