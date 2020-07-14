0fbf4dac20cee41e721586ed94fc78da05063a79
JK <saikitjk@gmail.com>
2020-07-14 13:32:22 -0700

    implemented all the features such as new array, new timestamps, new eventrows, new submit button  for the previous day feature

--- script.js
+++ script.js
@@ -1,4 +1,154 @@
 $(document).ready(function(){
+    //previousday
+    function previousDay(){
+        console.log("ggggg")
+        $("#eventContainer").hide();
+        $("#nextEventContainer").hide();
+        $("#prevEventContainer").show();
+        $("#currentDay").hide();
+        $("#currentTime").hide();
+        $("#nextDate").hide();
+        var prevDay = moment().subtract(1, "days").format("MMMM DD gggg, dddd");
+        var lastDay = $("#previousDate");
+        function headerClock(){
+        //Time date on the header
+        lastDay.text(prevDay);
+
+        }
+    
+        headerClock();
+        setInterval(headerClock,1000);
+        //Time date on the header
+    
+        //retrieve event
+        var prevEventArray = [];
+        prevSavedEvent = JSON.parse(localStorage.getItem("prevStoredArray"));
+        if(savedEvent !== null){
+            prevEventArray = prevSavedEvent;
+        }
+    
+        var prevEventRowContainer = $("#prevEventContainer");
+        prevEventRowContainer.empty();
+        
+    
+        for (var hour = 0; hour <= 24; hour++) {
+            //set index for labeling input box
+            var i = hour;
+            var j = hour;
+            if(j > 12){
+                j = j-12;
+            }
+            else if ( j === 0){
+                j = j+12;
+            }
+            //display time
+            var ampm = "";
+            if (hour > 12) { 
+                ampm = "PM";
+            } 
+            else {
+                ampm = "AM";
+            }
+            
+            var eventRow = $("<div>");
+            eventRow.addClass("row");
+            eventRow.addClass("eventRow");
+            eventRow.attr("row-index",hour);
+        
+            //build the column in eventContainer
+            var timeColumn = $("<div>");
+            timeColumn.addClass("col-md-2 col-sm-2");
+            var inputColumn = $("<div>");
+            inputColumn.addClass("col-md-9  col-sm-8");
+            var saveButtonColumn = $("<div>");
+            saveButtonColumn.addClass("col-md-1 col-sm-2");
+    
+            const timeStamp = $("<div>");
+            timeStamp.attr("class","timeStamp");
+    
+            //time stamp
+            timeStamp.text(j + " " + ampm);
+            timeStamp.css("background-color","#ebebeb");
+            eventRow.append(timeColumn);
+            timeColumn.append(timeStamp);
+        
+            //input box
+            var eventInputBox = $("<input>");
+            eventInputBox.attr("id", `inputBoxId${i}`);
+            eventInputBox.attr("boxIndex",i);
+            eventInputBox.attr("type","text");
+            eventInputBox.attr("class","eventBox");
+            eventInputBox.css("background-color","#ebebeb");
+            eventInputBox.css("opacity","0.6");
+            eventInputBox.val(prevEventArray[i]);
+            eventRow.append(inputColumn);
+            inputColumn.append(eventInputBox);
+    
+    
+            //save button
+            var saveButton = $("<button>");
+            saveButton.attr("id", `saveButtonId${i}`);
+            //console.log("gg" + index);
+            saveButton.attr("saveIndex",i);
+            saveButton.attr("class","far fa-save saveButton");
+            eventRow.append(saveButtonColumn);
+            saveButtonColumn.append(saveButton);
+    
+            
+            //append to html
+            prevEventRowContainer.append(eventRow);
+        
+           };//end of for loop
+    
+           //on click save button
+           $(document).on("click","button", function(event){
+                event.preventDefault();  
+                //var event2Save = document.getElementById("eventBox").value;
+                //console.log(event2Save);
+               
+    
+                var saveIndex = $(this).attr("saveIndex");
+                var inputId = "#inputBoxId" + saveIndex;
+                var value = $(inputId).val();
+                //console.log('value= ', value); 
+                //console.log('index= ', saveIndex); 
+                if(value === ""){
+                    return; //check if tany data
+                }
+            
+                prevEventArray[saveIndex] = value;
+    
+            //store to local
+            localStorage.setItem("prevStoredArray", JSON.stringify(prevEventArray));
+            });
+    
+            //onclick clear button
+            $("#clearEvent").on("click",function(){
+                //console.log("resetbutton worked")
+                resetEvent();
+            });
+    
+
+    
+            //adding reset button
+            function resetEvent(){
+                prevEventArray = [];
+                localStorage.setItem("prevStoredArray", JSON.stringify(prevEventArray));
+                //console.log("remove worked")
+                savedEvent = JSON.parse(localStorage.getItem("prevStoredArray"));
+                location.reload();
+            }
+    }
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+    function today(){
+    //$("#nextButtonContainer").hide();
+    $("#nextEventContainer").hide();
+    //$("#prevButtonContainer").hide();
+    $("#prevEventContainer").hide();
     var nowDay = moment().format("MMMM DD gggg, dddd");
     var nowTime = moment().format("LTS");
     var currentHour = moment().hour();
@@ -99,22 +249,26 @@ $(document).ready(function(){
 
     //build the columns in buttonContainer
     var leftColumn = $("<div>");
-    leftColumn.addClass("col-md-4 col-sm-3");
+    leftColumn.addClass("col-md-3 col-sm-3");
     var middleColumn = $("<div>");
-    middleColumn.addClass("col-md-4 col-sm-3");
+    middleColumn.addClass("col-md-3 col-sm-3");
     var rightColumn = $("<div>");
-    rightColumn.addClass("col-md-4 col-sm-3");
+    rightColumn.addClass("col-md-3 col-sm-3");
+    var clearColumn = $("<div>");
+    clearColumn.addClass("col-md-3 col-sm-3");
 
     //left button
     var leftButton = $("<button>");
     leftButton.attr("id", "previousDay"); 
     leftButton.attr("class","btn btn-primary");
+    leftButton.text("Yesterday");
     buttonRow.append(leftColumn);
     leftColumn.append(leftButton);
     //middle button
     var middleButton = $("<button>");
-    middleButton.attr("id", "clearEvent"); 
-    middleButton.attr("class","btn btn-warning");
+    middleButton.attr("id", "today"); 
+    middleButton.attr("class","btn btn-primary");
+    middleButton.text("Today");
     buttonRow.append(middleColumn);
     middleColumn.append(middleButton);
 
@@ -122,8 +276,17 @@ $(document).ready(function(){
     var rightButton = $("<button>");
     rightButton.attr("id", "nextDay"); 
     rightButton.attr("class","btn btn-primary");
+    rightButton.text("Tomorrow");
     buttonRow.append(rightColumn);
     rightColumn.append(rightButton);
+    
+    //clear button
+    var clearButton = $("<button>");
+    clearButton.attr("id", "clearEvent"); 
+    clearButton.attr("class","btn btn-warning");
+    clearButton.text("Clear All");
+    buttonRow.append(clearColumn);
+    clearColumn.append(clearButton);
 
     //append to buttonContainer
     buttonContainer.append(buttonRow)
@@ -154,6 +317,12 @@ $(document).ready(function(){
         $("#clearEvent").on("click",function(){
             //console.log("resetbutton worked")
             resetEvent();
+        });
+
+        $("#previousDay").on("click",function(){
+            console.log("previous day worked");
+            previousDay();
+            
         });
 
         function eventRowColor(){
@@ -188,12 +357,8 @@ $(document).ready(function(){
             location.reload();
         }
 
-        //adding previous day and next day
-   
-        
-
-
-
+    }
+    today();
 
 
 });
